f711b50ba21cec56f001b3a49d7aad1f581fe77e
tardisfromtornspace <andromedavirgoa@gmail.com>
2022-06-20 01:54:42 +0200

    Shadow fighters, and being capable of deploying the fighters from the abttlecrab directly or through a ball

---
+++ scripts/Custom/Ships/Shadow_FighterBall.py
@@ -0,0 +1,35 @@
+#####  Created by:
+#####  Bridge Commander Universal Tool
+
+
+import App
+import Foundation
+
+
+abbrev = "Shadow_FighterBall"
+iconName = "Shadow_FighterBall"
+longName = "Fighter Deploy"
+shipFile = "Shadow_FighterBall"
+species = App.SPECIES_SHUTTLE
+menuGroup = "Babylon 5"
+playerMenuGroup = "Babylon 5"
+SubMenu = "The Shadows"
+
+Foundation.ShipDef.Shadow_FighterBall = Foundation.ShipDef(abbrev, species, { 'name': longName, 'iconName': iconName, 'shipFile': shipFile, "SubMenu": SubMenu })
+
+Foundation.ShipDef.Shadow_FighterBall.dTechs = { "Phase Cloak": 10, 'Breen Drainer Immune': 1, 'Shadow Dispersive Hull': 1}
+
+Foundation.ShipDef.Shadow_FighterBall.desc = "The Shadow Battlecrabs don't throw Fighters one by one, but in a great bundled group."
+Foundation.ShipDef.Shadow_FighterBall.CloakingSFX = "shadowscream"
+Foundation.ShipDef.Shadow_FighterBall.DeCloakingSFX = "shadowscream"
+#                                                                                     #
+#######################################################################################
+#                                                                                     #
+# These register the ship with the QuickBattle menus.  Don't touch them!!!            #
+if menuGroup:           Foundation.ShipDef.Shadow_FighterBall.RegisterQBShipMenu(menuGroup)
+if playerMenuGroup:     Foundation.ShipDef.Shadow_FighterBall.RegisterQBPlayerShipMenu(playerMenuGroup)
+
+
+if Foundation.shipList._keyList.has_key(longName):
+      Foundation.ShipDef.__dict__[longName].friendlyDetails[2] = Foundation.shipList[longName].friendlyDetails[2]
+      Foundation.ShipDef.__dict__[longName].enemyDetails[2] = Foundation.shipList[longName].enemyDetails[2]
---
+++ scripts/ships/Shadow_Fighter4.py
@@ -0,0 +1,37 @@
+import App
+import Multiplayer.SpeciesToShip
+
+def GetShipStats():
+	kShipStats = { 
+		"FilenameHigh": "data/Models/Ships/Shadow_Fighter/Shadow_Fighter.nif",
+		"FilenameMed": "data/Models/Ships/Shadow_Fighter/Shadow_Fighter.nif",
+		"FilenameLow": "data/Models/Ships/Shadow_Fighter/Shadow_Fighter.nif",
+		"Name": "Shadow_Fighter4",
+		"HardpointFile": "Shadow_Fighter",
+		"Species": Multiplayer.SpeciesToShip.SHUTTLE
+	}
+	return kShipStats
+
+def LoadModel(bPreLoad = 0):
+	pStats = GetShipStats()
+
+	# Create the LOD info
+	if (not App.g_kLODModelManager.Contains(pStats["Name"])):
+		# Params are: File Name, PickLeafSize, SwitchOut Distance,
+		# Surface Damage Res, Internal Damage Res, Burn Value, Hole Value,
+		# Search String for Glow, Search string for Specular, Suffix for specular
+		pLODModel = App.g_kLODModelManager.Create(pStats["Name"])
+		pLODModel.AddLOD(pStats["FilenameHigh"], 10,  250.0, 0.0, 0.0, 8000, 0, "_glow", None, "_specular")
+		pLODModel.AddLOD(pStats["FilenameMed"],  10, 500.0, 0.0, 0.0, 8000, 0, "_glow", None, "_specular")
+		pLODModel.AddLOD(pStats["FilenameLow"],  10, 1000.0, 0.0, 0.0, 8000, 0, "_glow", None, None)
+
+#		kDebugObj = App.CPyDebug()
+		if (bPreLoad == 0):
+			pLODModel.Load()
+#			kDebugObj.Print("Loading " + pStats["Name"] + "\n")
+		else:
+			pLODModel.LoadIncremental()
+#			kDebugObj.Print("Queueing " + pStats["Name"] + " for pre-loading\n")
+
+def PreLoadModel():
+	LoadModel(1)
