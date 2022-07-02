82e2b2bfcada49db7e9056b38e52852cc34e90e2
tardisfromtornspace <andromedavirgoa@gmail.com>
2022-07-02 02:19:35 +0200

    Added Babylon 5 Earth Alliance Hyperion

---
+++ Documentation/B5ModsMainReadme.txt
@@ -0,0 +1,27 @@
+***Basically every other Babylon 5 ship mod has been overhauled by Alex SL Gato to include Jumpspace Drive, usually additional weapons and techs, and to balance each other - for example we had a Minbari cruiser to the level of a Sovereign and then we had a regular Omega which crushed easily several Minbari Sharlins, that won't do.***
+
+- Most meshes and textures not present on the other mods/readmes were taken from the Celestia Motherlode or remodeled by Alex SL Gato (tardisfromtornspace) when too complex for the tools used, with Blender and Milkshape.
+- Icons done by Alex SL Gato.
+- scripts: for the most part Alex SL Gato, except the Babylon 5 Jumpspace technology, which was adapted from the Slipstream's creator by Alex SL Gato
+- mod/ships/hardpoint: Alex SL Gato.
+- Special shadow cloak/decloak sound: taken by Alex SL Gato from the Babylon 5 season 4 intro.
+
+===Mods required===
+You'll of course need Foundation, Nanofx2, Submenu v 3.0, QBAutostart and Kobayashi Maru mod, the latest version the better. 
+
+===Installation Guide===
+1º Ok, first of all, just in case, backup your STBC.
+
+2º After that, just unzip the files into your STBC folder, say "yes" to overwrite all the files.
+
+3º After opening your STBC go to the options and activate "The Jumpspace" in start menu, and then activate all the new incorporated mutators (this mod uses foundation, all variants of Mvam, B5 uniforms, and Jumpspace, plus point defence, redistribute shields and repair destroyed systems mod. Ensure all of those Mutators are activated when playing to get the best experience of the B5 mod).
+
+
+--- Why those mutators are important ---
+
+Without the jumpspace mod active, none of the Younger Races ships can leave the system (to make it more canon to the series). Oh, and don't worry, I made it so the jumpspace engines can actually be engines so you can actually use emergency repair and repair destroyed systems properly on them... unlike the Hyperdrive or Slipstream mods.
+Foundation is vital because for most of the Babylon 5 races, there's at least one Foundation function working to make it more canonical. Without those functions the ship defences for most babylon 5 ships are pretty weak since they'll have no defence grids, point defence, gravitational defences, polycristalline armour... just the unmodded shield which was only used as a token to make the modded defences work.
+* That token shield is meant to be extremely weak and have little regeneration because:
+** Canonically, most Babylon 5 races don't have a bubble energy shield like Star Trek.
+** A strong bubble shield on Star Trek Bridge Commander prevents beam weapons from actually hitting the hull, unless you make them ridiculously powerful, to the point in order to make an Earth Alliance ship be capable of firing beams which can hit the hull of another Omega, they need to have a beam as powerful as the original Shadow Battlecrab mod - for a reference, that old mod could insta-kill the Armoured Voyager - and that won't do.
+** The token shield combined with the modded techs ensures that Beams actually are the most powerful weapon in Babylon 5, instead of a random person firing pulses o torpedoes - a shield in unmodded STBC actually recovers strength even while being hit, so beams against a fast-regenerating shield become almost worthless, while torpedoes and pulses give their payload damage in a single instant.
---
+++ scripts/Custom/Ships/EAHyperion.py
@@ -0,0 +1,69 @@
+#######################################################################################
+#  Custom Ship Plugin                                                                 #
+#  Created by BC - Mod Packager                                                       #
+#  Date: 16/04/2005                                                                    #
+#######################################################################################
+#                                                                                     #
+import Foundation
+import App
+#                                                                                     #
+#######################################################################################
+#                                                                                     #
+abbrev = 'EAHyperion'
+iconName = 'EAHyperion'
+longName = 'Hyperion Class'
+shipFile = 'EAHyperion' 
+menuGroup = 'Babylon 5'
+playerMenuGroup = 'Babylon 5'
+species = App.SPECIES_GALAXY
+SubMenu = "Earth Alliance"
+SubSubMenu = "Capital Ships"
+
+#                                                                                     #
+#######################################################################################
+#                                                                                     #
+# Mod Info.  Use this as an opportunity to describe your work in brief.  This may     #
+# have use later on for updates and such.                                             #
+#                                                                                     #
+credits = {
+	'modName': 'EAHyperion',
+	'author': 'Madjohn',
+	'version': '0.1beta',
+	'sources': [ 'http://' ],
+	'comments': ''
+}
+#                                                                                     #
+#######################################################################################
+#                                                                                     #
+# This is the ShipDef that adds the Ship to the game... BC-Mod Packager has           #
+# automatically generated the proper ShipDef Line for you.                            #
+#                                                                                     #
+Foundation.ShipDef.EAHyperion = Foundation.FedShipDef(abbrev, species, { 'name': longName, 'iconName': iconName, 'shipFile': shipFile, "SubMenu": SubMenu, "SubSubMenu": SubSubMenu })
+
+Foundation.ShipDef.EAHyperion.dTechs = {
+	'Defense Grid': 100
+}
+#                                                                                     #
+#######################################################################################
+#                                                                                     #
+# Uncomment these if you have TGL                                                     #
+#                                                                                     #
+# Foundation.ShipDef.EAHyperion.hasTGLName = 1
+# Foundation.ShipDef.EAHyperion.hasTGLDesc = 1
+#                                                                                     #
+# Otherwise, uncomment this and type something in:                                    #
+#                                                                                     #
+Foundation.ShipDef.EAHyperion.desc = "The Hyperion-class heavy cruiser was an Earthforce warship manufactured by KarmaTech, primarily at the Neue Hanse Orbital Shipyard in Earth orbit. Before the introduction of the Omega and Warlock-class destroyers, the Hyperion-class heavy cruisers served as Earth's front line warships, often supported by the heavily armed Nova-class dreadnought. Hyperion heavy cruisers made up a proportion of the fleet that was assembled for the Battle of the Line and though a great many were destroyed, the ones that survived continued to serve alongside their successors as the workhorses of the Earthforce fleet. Even though the design was far past its prime it continued to be in service as late as 2281. Like most Earth Alliance ships of the period, the Hyperion lacked the capability to generate artificial gravity, either through the use of rotating sections or the later, more advanced gravitic drive systems. Though rotating sections were within Earthforce's technical capabilities of the time, they greatly reduced a ship's speed and effectiveness. As such the crew had to operate in a completely zero gravity environment, requiring the use of seat straps and handholds to work effectively. Several decks are dedicated to crew quarters and facilities. Crew were often granted leave time after a tour to adapt to gravity again. In later years, ships such as the Clarkstown were seen upgraded with newer weaponry such as the 52 mm plasma pulse cannon used on the Omega class Destroyer."
+#                                                                                     #
+#######################################################################################
+#                                                                                     #
+# These register the ship with the QuickBattle menus.  Don't touch them!!!            #
+#                                                                                     #
+if menuGroup:           Foundation.ShipDef.EAHyperion.RegisterQBShipMenu(menuGroup)
+if playerMenuGroup:     Foundation.ShipDef.EAHyperion.RegisterQBPlayerShipMenu(playerMenuGroup)
+
+if Foundation.shipList._keyList.has_key(longName):
+     Foundation.ShipDef.__dict__[longName].friendlyDetails[2] = Foundation.shipList[longName].friendlyDetails[2]
+     Foundation.ShipDef.__dict__[longName].enemyDetails[2] = Foundation.shipList[longName].enemyDetails[2]
+#                                                                                     #
+#######################################################################################
