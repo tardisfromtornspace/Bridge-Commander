f6b6fcd37ffd668d9e66b27939a5af607faebab9
unknown <andromedavirgoa@gmail.com>
2023-04-25 22:42:59 +0200

    1920 res lcars, some changes

--- scripts/Custom/Ships/Sovereign.py
+++ scripts/Custom/Ships/Sovereign.py
@@ -4,7 +4,7 @@ Foundation.ShipDef.Sovereign.sBridge = 'SovereignBridge'
 Foundation.ShipDef.Sovereign.fMaxWarp = 9.99
 Foundation.ShipDef.Sovereign.dTechs = {
 	'Breen Drainer Immune': 1,
-	'Regenerative Shields': 1,
+	'Regenerative Shields': 100,
 	'Multivectral Shields': 10,
 	'Fed Ablative Armor': { "Plates": ["Aft Ablative Armor", "Engineering Ablative Armor", "Top Ablative Armor", "Forward Ablative Armor"]
 }}
