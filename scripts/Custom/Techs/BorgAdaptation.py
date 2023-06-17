da59c464444ecfef9e5fb5904b63695d58ea3033
unknown <andromedavirgoa@gmail.com>
2023-06-17 21:14:56 +0200

    Borg adaptation version 0.7

--- scripts/Custom/Techs/BorgAdaptation.py
+++ scripts/Custom/Techs/BorgAdaptation.py
@@ -1,7 +1,7 @@
 # THIS FILE IS NOT SUPPORTED BY ACTIVISION
 # THIS FILE IS UNDER THE LGPL FOUNDATION LICENSE AS WELL
-# 16 June 2023, by Alex SL Gato (CharaToLoki), partially based on the Shield.py script by the Foundation Technologies team and Dasher42's Foundation script
-# Version: 0.5
+# 17 June 2023, by Alex SL Gato (CharaToLoki), partially based on the Shield.py script by the Foundation Technologies team and Dasher42's Foundation script, and the FedAblativeArmor.py found in scripts/ftb/Tech in KM 2011.10
+# Version: 0.7
 #
 # TODO: 1. Create Read Me
 #	2. Create a clear guide on how to add this...
@@ -21,93 +21,99 @@ import Foundation
 import FoundationTech
 
 import nt
-
+import math
 import string
 
 maxCountdown = 65535 # we want to keep things moderate
+nonYieldAdaptationCounter = 100.0 # the point before the Borg manage to prevent 50% of the non-yield weapons damage
+extraReductionAdaptationCycle = 4 # this is how many extra steps are taken after nonYieldAdaptationCounter has been reached - the first initial ones make a huge drop
 
-# TO-DO ORDER ALPHABETICALLY AND NOT BY ADAPTING DIFFICULTY
+# THIS IS A GLOBAL LIST, WHICH WILL BE FILLED AUTOMATICALLY OR BY THE FILES IN scripts/Custom/Techs/BorgAdaptationsDefensive
 adaptationProgress = {
-"IsDrainYield" : App.g_kSystemWrapper.GetRandomNumber(12) + 0,                          # Immunity to breen drainer and similar drainers
-"Breen Damper" : App.g_kSystemWrapper.GetRandomNumber(12) + 0,                          
-"Damper Weapon" : App.g_kSystemWrapper.GetRandomNumber(12) + 0,                         
-"Breen Drainer Weapon" : App.g_kSystemWrapper.GetRandomNumber(12) + 0,
+#"IsDrainYield" : App.g_kSystemWrapper.GetRandomNumber(12) + 0,                          # Immunity to breen drainer and similar drainers
+#"Breen Damper" : App.g_kSystemWrapper.GetRandomNumber(12) + 0,                          
+#"Damper Weapon" : App.g_kSystemWrapper.GetRandomNumber(12) + 0,                         
+#"Breen Drainer Weapon" : App.g_kSystemWrapper.GetRandomNumber(12) + 0,
 
-"IsChainReactionPulsarYield" : App.g_kSystemWrapper.GetRandomNumber(90) + 20,          # Immunity to chain reaction pulsar
-"Chain Reaction Pulsar": App.g_kSystemWrapper.GetRandomNumber(90) + 20,
-"CRP Projectile": App.g_kSystemWrapper.GetRandomNumber(90) + 20,              
+#"IsChainReactionPulsarYield" : App.g_kSystemWrapper.GetRandomNumber(90) + 20,          # Immunity to chain reaction pulsar
+#"Chain Reaction Pulsar": App.g_kSystemWrapper.GetRandomNumber(90) + 20,
+#"CRP Projectile": App.g_kSystemWrapper.GetRandomNumber(90) + 20,              
 
-"IsChronitonYield" : App.g_kSystemWrapper.GetRandomNumber(160) + 60,                    # Immunity to Chroniton torpedoes
-"IsChronTorpYield" : App.g_kSystemWrapper.GetRandomNumber(160) + 60,
-"Chroniton Torp" : App.g_kSystemWrapper.GetRandomNumber(160) + 60,                      
-"ChronitonTorpe" : App.g_kSystemWrapper.GetRandomNumber(160) + 60,                      
-"Chroniton Torpedo" : App.g_kSystemWrapper.GetRandomNumber(160) + 60,                   
+#"IsChronitonYield" : App.g_kSystemWrapper.GetRandomNumber(160) + 60,                    # Immunity to Chroniton torpedoes
+#"IsChronTorpYield" : App.g_kSystemWrapper.GetRandomNumber(160) + 60,
+#"Chroniton Torp" : App.g_kSystemWrapper.GetRandomNumber(160) + 60,                      
+#"ChronitonTorpe" : App.g_kSystemWrapper.GetRandomNumber(160) + 60,                      
+#"Chroniton Torpedo" : App.g_kSystemWrapper.GetRandomNumber(160) + 60,                   
 
-"IsCloakDisablerYield" : App.g_kSystemWrapper.GetRandomNumber(12) + 0,                  # Immunity to all cloak disablers
-"Cloak Disabler" : App.g_kSystemWrapper.GetRandomNumber(12) + 0,
-"Cloak Disable" : App.g_kSystemWrapper.GetRandomNumber(12) + 0,                  
+#"IsCloakDisablerYield" : App.g_kSystemWrapper.GetRandomNumber(12) + 0,                  # Immunity to all cloak disablers
+#"Cloak Disabler" : App.g_kSystemWrapper.GetRandomNumber(12) + 0,
+#"Cloak Disable" : App.g_kSystemWrapper.GetRandomNumber(12) + 0,                  
 
-"IsComputerVirusYield" : App.g_kSystemWrapper.GetRandomNumber(60) + 0,                 # Immunity to computer viruses
-"Computer Virus" : App.g_kSystemWrapper.GetRandomNumber(60) + 0,                 
-"PCVirus Weapon', " : App.g_kSystemWrapper.GetRandomNumber(60) + 0,
+#"IsComputerVirusYield" : App.g_kSystemWrapper.GetRandomNumber(60) + 0,                 # Immunity to computer viruses
+#"Computer Virus" : App.g_kSystemWrapper.GetRandomNumber(60) + 0,                 
+#"PCVirus Weapon', " : App.g_kSystemWrapper.GetRandomNumber(60) + 0,
 
-"IsElectromagneticDisablerYield" : App.g_kSystemWrapper.GetRandomNumber(5) + 0,        # Immunity against ElectroMagnetic attacks
-"Electro-Magnetic Pulse" : App.g_kSystemWrapper.GetRandomNumber(5) + 0,        
-"EMP Projectile" : App.g_kSystemWrapper.GetRandomNumber(5) + 0,
+#"IsElectromagneticDisablerYield" : App.g_kSystemWrapper.GetRandomNumber(5) + 0,        # Immunity against ElectroMagnetic attacks
+#"Electro-Magnetic Pulse" : App.g_kSystemWrapper.GetRandomNumber(5) + 0,        
+#"EMP Projectile" : App.g_kSystemWrapper.GetRandomNumber(5) + 0,
 
-"IsImpulseDisablerYield" : App.g_kSystemWrapper.GetRandomNumber(11) + 0,                # Immunity to all impulse disablers
-"Impulse Disabler" : App.g_kSystemWrapper.GetRandomNumber(11) + 0,
-"Impulse Disable" : App.g_kSystemWrapper.GetRandomNumber(11) + 0,                
+#"IsImpulseDisablerYield" : App.g_kSystemWrapper.GetRandomNumber(11) + 0,                # Immunity to all impulse disablers
+#"Impulse Disabler" : App.g_kSystemWrapper.GetRandomNumber(11) + 0,
+#"Impulse Disable" : App.g_kSystemWrapper.GetRandomNumber(11) + 0,                
 
-"IsIonProjectileYield" : App.g_kSystemWrapper.GetRandomNumber(20) + 0,                  # Immunity to all ion weapons
-"IsIonWeaponYield" : App.g_kSystemWrapper.GetRandomNumber(20) + 0,                      
-"Ion Projectile" : App.g_kSystemWrapper.GetRandomNumber(20) + 0,             
-"Ion Weapon" : App.g_kSystemWrapper.GetRandomNumber(20) + 0,  
+#"IsIonProjectileYield" : App.g_kSystemWrapper.GetRandomNumber(20) + 0,                  # Immunity to all ion weapons
+#"IsIonWeaponYield" : App.g_kSystemWrapper.GetRandomNumber(20) + 0,                      
+#"Ion Projectile" : App.g_kSystemWrapper.GetRandomNumber(20) + 0,             
+#"Ion Weapon" : App.g_kSystemWrapper.GetRandomNumber(20) + 0,  
 
-"IsIsokineticYield" : App.g_kSystemWrapper.GetRandomNumber(8) + 2,                     # Immunity to all kinds of isokinetic attacks, including isokinetic cannon rounds
-"Isokinetic Cannon Round" : App.g_kSystemWrapper.GetRandomNumber(8) + 2,
+#"IsIsokineticYield" : App.g_kSystemWrapper.GetRandomNumber(8) + 2,                     # Immunity to all kinds of isokinetic attacks, including isokinetic cannon rounds
+#"Isokinetic Cannon Round" : App.g_kSystemWrapper.GetRandomNumber(8) + 2,
 
-"IsMultipleDisablerYield" : App.g_kSystemWrapper.GetRandomNumber(32) + 5,              # Immunity to all multiple disablers
-"Multiple Disable" : App.g_kSystemWrapper.GetRandomNumber(32) + 5,
+#"IsMultipleDisablerYield" : App.g_kSystemWrapper.GetRandomNumber(32) + 5,              # Immunity to all multiple disablers
+#"Multiple Disable" : App.g_kSystemWrapper.GetRandomNumber(32) + 5,
 
-"IsNanoprobeYield" : App.g_kSystemWrapper.GetRandomNumber(4) + 0,                      # Immunity to nanoprobes
-"Nanoprobe Projectile" : App.g_kSystemWrapper.GetRandomNumber(4) + 0,
+#"IsNanoprobeYield" : App.g_kSystemWrapper.GetRandomNumber(4) + 0,                      # Immunity to nanoprobes
+#"Nanoprobe Projectile" : App.g_kSystemWrapper.GetRandomNumber(4) + 0,
 
-"Phalantium Wave" : App.g_kSystemWrapper.GetRandomNumber(52) + 160,                    # Immunity to Phalantium Wave
+#"Phalantium Wave" : App.g_kSystemWrapper.GetRandomNumber(52) + 160,                    # Immunity to Phalantium Wave
 
-"IsPhaseYield" : App.g_kSystemWrapper.GetRandomNumber(100) + 200,                       # Immunity to standard Phased weaponry (specifically torpedoes)
-"IsPhasedYield" : App.g_kSystemWrapper.GetRandomNumber(100) + 200,                      # Immunity to one alternative Phased Weaponry (probably a typo, better keep it)
-"Phased Torpedo" : App.g_kSystemWrapper.GetRandomNumber(100) + 200,
+#"IsPhaseYield" : App.g_kSystemWrapper.GetRandomNumber(100) + 200,                       # Immunity to standard Phased weaponry (specifically torpedoes)
+#"IsPhasedYield" : App.g_kSystemWrapper.GetRandomNumber(100) + 200,                      # Immunity to one alternative Phased Weaponry (probably a typo, better keep it)
+#"Phased Torpedo" : App.g_kSystemWrapper.GetRandomNumber(100) + 200,
 
-"IsPowerDisablerYield" : App.g_kSystemWrapper.GetRandomNumber(14) + 2,                  # Immunity to all power disablers
-"Power Disable" : App.g_kSystemWrapper.GetRandomNumber(14) + 2,
+#"IsPowerDisablerYield" : App.g_kSystemWrapper.GetRandomNumber(14) + 2,                  # Immunity to all power disablers
+#"Power Disable" : App.g_kSystemWrapper.GetRandomNumber(14) + 2,
 
-"IsRandomDisablerYield" : App.g_kSystemWrapper.GetRandomNumber(16) + 0,                 # Immunity to all strange disablers
-"Random Disable" : App.g_kSystemWrapper.GetRandomNumber(16) + 0,
+#"IsRandomDisablerYield" : App.g_kSystemWrapper.GetRandomNumber(16) + 0,                 # Immunity to all strange disablers
+#"Random Disable" : App.g_kSystemWrapper.GetRandomNumber(16) + 0,
 
-"IsRefluxWeaponYield" : App.g_kSystemWrapper.GetRandomNumber(100) + 100,                 # Immunity to Reflux weaponry (specifically torpedoes)
+#"IsRefluxWeaponYield" : App.g_kSystemWrapper.GetRandomNumber(100) + 100,                 # Immunity to Reflux weaponry (specifically torpedoes)
 
-"IsSensorDisablerYield" : App.g_kSystemWrapper.GetRandomNumber(10) + 0,                 # Immunity to all sensor disablers
-"Sensor Disable" : App.g_kSystemWrapper.GetRandomNumber(10) + 0,
+#"IsSensorDisablerYield" : App.g_kSystemWrapper.GetRandomNumber(10) + 0,                 # Immunity to all sensor disablers
+#"Sensor Disable" : App.g_kSystemWrapper.GetRandomNumber(10) + 0,
 
-"IsShieldDisablerYield" : App.g_kSystemWrapper.GetRandomNumber(10) + 0,                 # Immunity to all shield disablers
-"Shield Disable" : App.g_kSystemWrapper.GetRandomNumber(10) + 0,
+#"IsShieldDisablerYield" : App.g_kSystemWrapper.GetRandomNumber(12) + 0,                 # Immunity to all shield disablers
+#"Shield Disable" : App.g_kSystemWrapper.GetRandomNumber(12) + 0,
 
-"Spatial Charge" : App.g_kSystemWrapper.GetRandomNumber(40) + 40,                      # Immunity to Spatial Charge
+#"Spatial Charge" : App.g_kSystemWrapper.GetRandomNumber(40) + 40,                      # Immunity to Spatial Charge
 
-"IsTachyonProjectileYield" : App.g_kSystemWrapper.GetRandomNumber(120) + 0,             # Immunity against Tachyon weapons
-"Tachyon Weapon" : App.g_kSystemWrapper.GetRandomNumber(120) + 0,
+#"IsTachyonProjectileYield" : App.g_kSystemWrapper.GetRandomNumber(120) + 0,             # Immunity against Tachyon weapons
+#"Tachyon Weapon" : App.g_kSystemWrapper.GetRandomNumber(120) + 0,
 
-"IsTimeVortexYield" : App.g_kSystemWrapper.GetRandomNumber(1200) + 30000,               # Immunity against Time Vortex weaponry
+#"IsTimeVortexYield" : App.g_kSystemWrapper.GetRandomNumber(1200) + 30000,               # Immunity against Time Vortex weaponry
 
-"IsTransphasicYield" : App.g_kSystemWrapper.GetRandomNumber(300) + 300,                # Immunity against Transphasic technology (specifically Transphasic Torpedoes)
+#"IsTransphasicYield" : App.g_kSystemWrapper.GetRandomNumber(300) + 400,                # Immunity against Transphasic technology (specifically Transphasic Torpedoes)
 
-"IsWarpDisablerYield" : App.g_kSystemWrapper.GetRandomNumber(14) + 0,                   # Immunity to all warp disablers
-"Warp Disable" : App.g_kSystemWrapper.GetRandomNumber(14) + 0,
+#"IsWarpDisablerYield" : App.g_kSystemWrapper.GetRandomNumber(14) + 0,                   # Immunity to all warp disablers
+#"Warp Disable" : App.g_kSystemWrapper.GetRandomNumber(14) + 0,
 
 
 # TO-DO this is for offensive, leaving here just in case
 #"Dicohesive Tech Shields": App.g_kSystemWrapper.GetRandomNumber(100) + 100, 
+
+}
+
+normalWeaponAdaptation = {
 
 }
 
@@ -126,10 +132,16 @@ _g_dExcludeBorgPlugins = {
 	"000-Utilities-GetFolderNames-20040326": 1,
 }
 
+# based on the FedAblativeArmour.py script, a fragment probably imported from ATP Functions by Apollo
+def NiPoint3ToTGPoint3(p):
+	kPoint = App.TGPoint3()
+	kPoint.SetXYZ(p.x, p.y, p.z)
+	return kPoint
+
 # Based on LoadExtraPlugins by Dasher42, but heavily modified so it only imports a thing
 def LoadExtraLimitedPlugins(dExcludePlugins=_g_dExcludeBorgPlugins):
 
-	dir="scripts\\Custom\\Techs\\BorgAdaptationsDefensive" # I want to limit any vulnerabiltiy as much as I can while keeping functionality
+	dir="scripts\\Custom\\Techs\\BorgAdaptationsDefensive" # I want to limit any vulnerability as much as I can while keeping functionality
 	import string
 
 	list = nt.listdir(dir)
@@ -148,7 +160,7 @@ def LoadExtraLimitedPlugins(dExcludePlugins=_g_dExcludeBorgPlugins):
 
 		# We don't want to accidentally load wrong things
 		if (extension == "py") and not fileName == "__init__": # I am not allowing people to just use the .pyc directly, I don't want people to not include source scripts - Alex SL Gato
-			print "Borg are reviewing " + fileName + " of dir " + dir
+			#print "Borg are reviewing " + fileName + " of dir " + dir
 			if dExcludePlugins.has_key(fileName):
 				debug(__name__ + ": Ignoring outdated plugin" + fileName)
 				continue
@@ -175,18 +187,27 @@ def LoadExtraLimitedPlugins(dExcludePlugins=_g_dExcludeBorgPlugins):
 					# This standard is to call the file as "IsXXXXYield", with XXXX being the tech, that is "IsDrainYield", "IsPhasedYield", etc. Or call it like the Yield name (f.ex. "Hopping Torpedo")
 					adaptationProgress[fileName] =  trueRandomness + constantness
 					
-					print "Borg reviewing of this tech is a success, adapting at the following number of shots: " + str(adaptationProgress[fileName])
+					#print "Borg reviewing of this tech is a success, adapting at the following number of shots: " + str(adaptationProgress[fileName])
 			except:
-				print "someone attempted to add more than they should"
+				print "someone attempted to add more than they should to the Borg Adaptation script"
 				traceback.print_exc()
 
 
 LoadExtraLimitedPlugins()
+#print adaptationProgress
 
 
 class BorgAdaptationDef(FoundationTech.TechDef):
 
 	def OnDefense(self, pShip, pInstance, oYield, pEvent, itemName):
+
+		# let's make it so people can customize how fast can a borg ship adapt, within reason
+		learningFactor = pInstance.__dict__['Borg Adaptation']
+		if learningFactor > maxCountdown:
+			learningFactor = maxCountdown
+		elif learningFactor < -maxCountdown:
+			learningFactor = -maxCountdown
+
 		if oYield:
 			import traceback
 			try:
@@ -196,6 +217,7 @@ class BorgAdaptationDef(FoundationTech.TechDef):
 						sameYield = (0 == 1) # For some reason it doesn't recognize False like that :/
 					else:
 						sameYield = (oYield == FoundationTech.oTechs[attre])
+
 					if (hasattr(oYield, str(attre)) and getattr(oYield, str(attre))()) or sameYield:
 						if not adaptationProgress.has_key(attre):
 							adaptationProgress[attre] = 800
@@ -206,13 +228,6 @@ class BorgAdaptationDef(FoundationTech.TechDef):
 						elif adaptationProgress[attre] > maxCountdown:
 							adaptationProgress[attre] = maxCountdown
 
-						# let's make it so people can customize how fast can a borg ship adapt, within reason
-						learningFactor = pInstance.__dict__['Borg Adaptation']
-						if learningFactor > maxCountdown:
-							learningFactor = maxCountdown
-						elif learningFactor < -maxCountdown:
-							learningFactor = -maxCountdown
-
 						adaptationProgress[attre] = adaptationProgress[attre] - learningFactor
 
 				if not (itemName == "None whatsoever") and not adaptationProgress.has_key(oYield):
@@ -220,11 +235,92 @@ class BorgAdaptationDef(FoundationTech.TechDef):
 			except:
 				print "something went wrong with Borg Adaptation technology"
 				traceback.print_exc()
-			# TO-DO add something for raw damage?
+		else:
+			import traceback
+			try:
+				if not (itemName == "None whatsoever"):
+					if not normalWeaponAdaptation.has_key(itemName):
+						normalWeaponAdaptation[itemName] = 0
+
+					if pEvent.IsHullHit():
+						# damage values
+						fRadius = pEvent.GetRadius()
+						fDamage = pEvent.GetDamage()
+
+						# get the systems
+						lSystems = []
+						kIterator = pShip.StartGetSubsystemMatch(App.CT_SHIP_SUBSYSTEM)
+						while (1):
+							pSubsystem = pShip.GetNextSubsystemMatch(kIterator)
+							if not pSubsystem:
+								break
+
+							lSystems.append(pSubsystem)
+						
+							for i in range(pSubsystem.GetNumChildSubsystems()):
+								pChild = pSubsystem.GetChildSubsystem(i)
+								lSystems.append(pChild)
+
+						pShip.EndGetSubsystemMatch(kIterator)
+
+						# get affected systems
+						lAffectedSystems = []
+						kPoint = NiPoint3ToTGPoint3(pEvent.GetObjectHitPoint())
+						for pSystem in lSystems:
+							vDifference = NiPoint3ToTGPoint3(pSystem.GetPosition())
+							vDifference.Subtract(kPoint)
+
+							if pSystem.GetRadius() > vDifference.Length():
+								lAffectedSystems.append(pSystem)
+
+						# print lAffectedSystems
+						if len(lAffectedSystems) > 0:
+							normalWeaponAdaptation[itemName] = normalWeaponAdaptation[itemName] + learningFactor
+							if normalWeaponAdaptation[itemName] < -10:
+								normalWeaponAdaptation[itemName] = -10
+							elif normalWeaponAdaptation[itemName] > (nonYieldAdaptationCounter + extraReductionAdaptationCycle):
+								normalWeaponAdaptation[itemName] = nonYieldAdaptationCounter + extraReductionAdaptationCycle
+
+						# calculate the damage per radius
+						fOffSet = 1 + fRadius
+						fAllocatedFactor = fOffSet
+						if fAllocatedFactor < 1:
+							fAllocatedFactor = 1
+
+						countDamage1 = ((nonYieldAdaptationCounter - normalWeaponAdaptation[itemName]) / nonYieldAdaptationCounter)
+						if countDamage1 > 0:
+							countDamage2 = countDamage1 ** (1.0/3.0)
+						elif countDamage1 < 0:
+							countDamage2 = -((-countDamage1) ** (1.0/3.0))
+						else: # In case your python doesn't support this or makes it work differently
+							countDamage2 = 0
+
+						damageMultiplier = (countDamage2  + 1.0) * 0.5
+						# print "dmg multiplier for " + str(itemName) + " stage " + str(normalWeaponAdaptation[itemName]) + " with " + str(countDamage1) + " and " + str(countDamage2) + ": " + str(damageMultiplier)
+						damageHealed = (fDamage * ( 1 - damageMultiplier))
+						# print "Event radius: " + str(fRadius)
+						for pSystem in lAffectedSystems:
+							status = pSystem.GetConditionPercentage()
+							# print "status" + str(status)
+							if status > 0:
+								#fNewCondition = status + (damageHealed / pSystem.GetMaxCondition()) * fAllocatedFactor / len(lAffectedSystems)
+								fNewCondition = status + (damageHealed / pSystem.GetMaxCondition()) * fAllocatedFactor / len(lAffectedSystems)
+								# print "fNewCondition = " + str(fNewCondition)
+							
+								if fNewCondition < 0:
+									fNewCondition = 0
+								elif fNewCondition > 1:
+									fNewCondition = 1
+								pSystem.SetConditionPercentage(fNewCondition)
+					#else:	
+						
+			except:
+				print "something went wrong with Borg Adaptation technology"
+				traceback.print_exc()
 
 	def OnBeamDefense(self, pShip, pInstance, oYield, pEvent):
 		try:
-			pEmitter = App.TractorBeamProjector_Cast(pEvent.GetSource())
+			pEmitter = App.PhaserBank_Cast(pEvent.GetSource())
 			itemName = pEmitter.GetFireSound()
 		except:
 			itemName = "None whatsoever"
