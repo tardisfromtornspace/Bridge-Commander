38db80f801b26ab2b632139fa0ef0ac6a6165c70
unknown <andromedavirgoa@gmail.com>
2024-11-03 18:37:19 +0100

    Fixed one of the races having a plasma and warp fx issue

--- scripts/Custom/Autoload/RaceTMEGXFH.py
+++ scripts/Custom/Autoload/RaceTMEGXFH.py
@@ -7,10 +7,10 @@ class TMEGXFHShipDef(Foundation.ShipDef):
         dict['race'] = oTMEGXFHRace
         Foundation.ShipDef.__init__(self, abbrev, species, dict)
 		
-    def OverrideWarpFXColor(self):
+    def OverrideWarpFXColor(self, *args):
         return (10.0, 175.0, 25.0)
 	
-    def OverridePlasmaFXColor(self):
+    def OverridePlasmaFXColor(self, *args):
         return (255.0, 0.0, 0.0)
 
 Foundation.TMEGXFHRace = oTMEGXFHRace
