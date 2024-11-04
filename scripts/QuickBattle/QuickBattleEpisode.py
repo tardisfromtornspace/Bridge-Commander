5281a4fd298b15488cfd4bbfecbf2c32ee5c0d8b
unknown <andromedavirgoa@gmail.com>
2024-11-04 22:20:14 +0100

    Fixed the fact that QuickBattleEpisode was missing PreLoadAssets

--- scripts/QuickBattle/QuickBattleEpisode.py
+++ scripts/QuickBattle/QuickBattleEpisode.py
@@ -10,7 +10,18 @@ import App
 #
 # This is where you would put Episode level module globals
 #
-
+###############################################################################
+#	PreLoadAssets()
+#	
+#	This is called once, at the beginning of the mission before Initialize()
+#	to allow us to add models to be pre loaded
+#	
+#	Args:	pMission	- the Mission object
+#	
+#	Return:	none
+###############################################################################
+def PreLoadAssets(pMission):
+	return
 
 #
 # Initialize our Episode
