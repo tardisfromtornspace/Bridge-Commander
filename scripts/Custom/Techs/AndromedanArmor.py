2123f927cace66472bc1fea27e12fade2da6f165
unknown <andromedavirgoa@gmail.com>
2026-01-26 21:37:11 +0100

    Updated andromedan armor to 0.13, adding armor strength support and fixing a small goof with one parameter

--- scripts/Custom/Techs/AndromedanArmor.py
+++ scripts/Custom/Techs/AndromedanArmor.py
@@ -1,7 +1,7 @@
 # THIS FILE IS NOT SUPPORTED BY ACTIVISION
 # THIS FILE IS UNDER THE FOUNDATION LGPL LICENSE AS WELL
 # Andromedan.py
-# 15th January 2026, by Alex SL Gato, based on a Cristalline armour adjusted for colors, fixed by Alex SL Gato (CharaToLoki), modified from Greystar's (which was likely an ftb AblativeArmour script copy)
+# 26th January 2026, by Alex SL Gato, based on a Cristalline armour adjusted for colors, fixed by Alex SL Gato (CharaToLoki), modified from Greystar's (which was likely an ftb AblativeArmour script copy)
 # Also based on SlowStart.py by Alex SL Gato.
 #
 # Meant to be used alongside ftb AblativeArmour, in fact, it requires it to work properly. It also requires to have the FIX-AblativeArmour1dot0 too to work properly.
@@ -24,13 +24,14 @@ Foundation.ShipDef.saturn.dTechs = {
 # - "GlobalFactor" determines how much they recharge (it's a multiplier of the event damage).
 # - "HealDepletes" is also a multiplier, which indicates if a healing weapon will depower these instead (1), if it will ignore it (0) or if it will replenish energy too (-1), or if it will gain more or something in-between.
 # - "Beams" lists a bunch of phaser beam properties, and "Pulses" does the same for pulse firing properties. If these fields do not apepar, it will assume all beams and all pusles can benefit from thsi tech.
+# - "DmgStrMod" and "DmgRadMod" affect how much is the visible mesh affected when hit, multiplying, the visible strength of the impact and the visible radious when the armour hardpoint is not dead. If neither of them are there, it will not modify any visible armour setting.
 Foundation.ShipDef.saturn.dTechs = {
-	'Andromedan-type Armor': [485000, {"Beams": ["One phaser hardpoint name", "another laser hardpoint name"], "Pulses": ["One pulse hardpoint name", "Another disruptor hardpoint name"], "GlobalFactor": 1.0, "HealDepletes": 0}]
+	'Andromedan-type Armor': [485000, {"Beams": ["One phaser hardpoint name", "another laser hardpoint name"], "Pulses": ["One pulse hardpoint name", "Another disruptor hardpoint name"], "GlobalFactor": 1.0, "HealDepletes": 0, "DmgStrMod": 0.0, "DmgRadMod": 0.0}]
 }
 
 # If you want to do everything at once, do this:
 Foundation.ShipDef.saturn.dTechs = {
-	'Andromedan-type Armor': [485000, "Potato tasty chip", {"Beams": ["One phaser hardpoint name", "another laser hardpoint name"], "Pulses": ["One pulse hardpoint name", "Another disruptor hardpoint name"], "GlobalFactor": 1.0, "HealDepletes": 0}]
+	'Andromedan-type Armor': [485000, "Potato tasty chip", {"Beams": ["One phaser hardpoint name", "another laser hardpoint name"], "Pulses": ["One pulse hardpoint name", "Another disruptor hardpoint name"], "GlobalFactor": 1.0, "HealDepletes": 0, "DmgStrMod": 0.0, "DmgRadMod": 0.0}]
 }
 """
 # SECOND, you need to add a hardpoint health property (could be a hull subsystem, but can also be another kind of subsystem... anything with actual health, no OEP of blinking light properties, please!) on that ships's script/ships/Hardpoints/whatever.py file with the same name as those used on the scripts/Custom/Ships/whateverShip.py one.
@@ -39,7 +40,7 @@ Foundation.ShipDef.saturn.dTechs = {
 ##################################
 #
 MODINFO = { "Author": "\"ftb Team\", \"Apollo\", \"Greystar\", \"Alex SL Gato\" (andromedavirgoa@gmail.com)",
-	    "Version": "0.1",
+	    "Version": "0.13",
 	    "License": "LGPL",
 	    "Description": "Read the small title above for more info"
 	    }
@@ -48,9 +49,11 @@ MODINFO = { "Author": "\"ftb Team\", \"Apollo\", \"Greystar\", \"Alex SL Gato\" 
 #
 import App
 from bcdebug import debug
+import traceback
+
 import Foundation
 import FoundationTech
-import traceback
+
 
 # TO-DO PLEASE AFTER TESTING TELL ME ANY PROBLEMS SO I CAN FIX THEM, PLUS ADD THE ACCESIBILITY FEATURE!
 kEmptyColor = App.TGColorA()
@@ -69,6 +72,25 @@ except:
 		AblativeArmour = None
 
 if AblativeArmour != None:
+
+	def findShipInstance(pShip):
+		debug(__name__ + ", findShipInstance")
+		pInstance = None
+		try:
+			if not pShip:
+				return pInstance
+			if FoundationTech.dShips.has_key(pShip.GetName()):
+				pInstance = FoundationTech.dShips[pShip.GetName()]
+		except:
+			pass
+		return pInstance
+
+	def GetWellShipFromID(pShipID):
+		pShip = App.ShipClass_GetObjectByID(App.SetClass_GetNull(), pShipID)
+		if not pShip or pShip.IsDead() or pShip.IsDying():
+			return None
+		return pShip
+
 	parentOnDefense = AblativeArmour.AblativeDef.OnDefense
 	#parentAttach = AblativeArmour.AblativeDef.Attach
 	class AndromedanArmorDef(AblativeArmour.AblativeDef):
@@ -120,6 +142,8 @@ if AblativeArmour != None:
 
 							pInstanceDict[self.lName][2] = pInstance.__dict__[str(self.name)][2]
 							pInstanceDict[self.lNameOld][2] = pInstance.__dict__[str(self.name)][2]
+						
+						self.conditionalVisibleDmgSwitch(pInstance, None, 1.0)
 					else:
 						pInstanceDict[self.lName] = pInstance.__dict__[str(self.name)][0]
 						pInstanceDict[self.lNameOld] = pInstance.__dict__[str(self.name)][0]
@@ -127,6 +151,85 @@ if AblativeArmour != None:
 					pInstanceDict[self.lName] = pInstance.__dict__[str(self.name)]
 					pInstanceDict[self.lNameOld] = pInstance.__dict__[str(self.name)]
 
+
+		def conditionalVisibleDmgSwitch(self, pInstance, pShip=None, repair=-1.0):
+			try:
+				if pInstance != None:
+					pInstanceDict = pInstance.__dict__
+					if hasattr(self, "lName") and hasattr(self, "lNameOld") and pInstanceDict.has_key(self.lName) and pInstanceDict.has_key(self.lNameOld):
+						if str(pInstanceDict[str(self.name)])[0] == "[":
+							myLen = len(pInstanceDict[str(self.name)])
+							if myLen > 2:
+								setDmgStrMod = "None"
+								setDmgRadMod = "None"
+								if pInstanceDict[self.lName][2].has_key("DmgStrMod"):
+									setDmgStrMod = pInstanceDict[self.lName][2]["DmgStrMod"]
+
+								if pInstanceDict[self.lName][2].has_key("DmgRadMod"):
+									setDmgRadMod = pInstanceDict[self.lName][2]["DmgRadMod"]
+
+								if (setDmgStrMod != None and setDmgStrMod != "None") or (setDmgRadMod != None and setDmgRadMod != "None"):
+									iShipID = None
+									if pShip != None and hasattr(pShip, "GetObjID"):
+										iShipID = pShip.GetObjID()
+									elif pInstance != None and hasattr(pInstance, "pShipID"):
+										iShipID = pInstance.pShipID
+
+									pShip2 = GetWellShipFromID(iShipID)
+									if pShip2 != None:
+										if repair > 0.0:
+											self.SetDmgRadMod(pInstance, pShip2, setDmgRadMod, setDmgStrMod)
+										else:
+											self.SetDmgRadMod(pInstance, pShip2, None, None)
+			except:
+				print __name__, "Error on conditionalVisibleDmgSwitch:"
+				traceback.print_exc()
+
+
+		def SetDmgRadModif(self, pShip, dmgRd=None, dmgStr=None):
+			myRad = 1.0
+			myStr = 1.0
+			pShipModule = None
+			if pShip and hasattr(pShip, "GetScript"):
+				pShipModule = __import__(pShip.GetScript())
+
+			if not pShipModule:
+				print "WARNING: Some vessel does not have a ship module???"
+				return
+			try:
+				
+				kStats=pShipModule.GetShipStats()
+				if (kStats.has_key('DamageRadMod')):
+					myRad = kStats['DamageRadMod']
+
+				if (kStats.has_key('DamageStrMod')):
+					myStr = kStats['DamageStrMod']
+			except:
+				print __name__, " Warning on SetDmgRadModif: "
+				traceback.print_exc()
+				try:
+					if hasattr(pShipModule, "GetDamageRadMod"):
+						myRad = pShipModule.GetDamageRadMod()
+				except:
+					myRad = 1.0
+
+				try:
+					if hasattr(pShipModule, "GetDamageStrMod"):
+						myStr = pShipModule.GetDamageStrMod()
+				except:
+					myStr = 1.0
+
+			if dmgRd == None:
+				pShip.SetVisibleDamageRadiusModifier(myRad)
+			elif dmgRd != "None":
+				pShip.SetVisibleDamageRadiusModifier(dmgRd)				
+
+			if dmgStr == None:
+				pShip.SetVisibleDamageStrengthModifier(myStr)
+			elif dmgStr != "None":
+				pShip.SetVisibleDamageStrengthModifier(dmgStr)	
+
+			
 			
 		def OnDefense(self, pShip, pInstance, oYield, pEvent):
 			
@@ -149,7 +252,7 @@ if AblativeArmour != None:
 
 				if validSubsystems.has_key("HealDepletes"):
 					if pEvent.GetDamage() < 0.0:
-						rechargeIs = rechargeIs * validSubsystems["GlobalFactor"]
+						rechargeIs = rechargeIs * validSubsystems["HealDepletes"]
 					
 				if validSubsystems.has_key("Beams"):
 					if len(validSubsystems["Beams"]) > 0:
@@ -192,6 +295,12 @@ if AblativeArmour != None:
 						print "TO-DO: I do not have pulses key, I will assume all pulses have ", __name__, " ability"
 						pulses = pShip.GetPulseWeaponSystem()
 						self.setChildrenSubsystemsChargeTo0(pulses)
+
+			repairV = -1
+			if repair != None and (type(1.0) == type(repair) or type(1) == type(repair)):
+				repairV = repair
+
+			self.conditionalVisibleDmgSwitch(pInstance, pShip, repairV)
 
 			parentOnDefense(self, pShip, pInstance, oYield, pEvent)
 
